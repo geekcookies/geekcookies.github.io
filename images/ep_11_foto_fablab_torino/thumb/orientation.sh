b6a0bc94b48701ad4f958eec8ae27955b365f7f8
Mario D Amore <mariodamore@pbopheliaintradlrde.local>
2015-11-10 13:26:12 +0100

    ep 11 post plus images

---
+++ images/ep_11_foto_fablab_torino/thumb/orientation.sh
@@ -0,0 +1,7 @@
+
+exiftool-5.16 -overwrite_original -n -Orientation=1 IMG_1279.thumb.JPG
+exiftool-5.16 -overwrite_original -n -Orientation=2 IMG_1280.thumb.JPG
+exiftool-5.16 -overwrite_original -n -Orientation=3 IMG_1281.thumb.JPG
+exiftool-5.16 -overwrite_original -n -Orientation=4 IMG_1282.thumb.JPG
+exiftool-5.16 -overwrite_original -n -Orientation=5 IMG_1283.thumb.JPG
+exiftool-5.16 -overwrite_original -n -Orientation=6 IMG_1284.thumb.JPG
