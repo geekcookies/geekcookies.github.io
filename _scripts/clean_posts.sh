66b637afb43aa7f7a5fadad89461686e4becd3a4
kidpixo <kidpixo@gmail.com>
2016-07-19 20:04:13 +0200

    updated the google dirty-link regex for vim

--- _scripts/clean_posts.sh
+++ _scripts/clean_posts.sh
@@ -4,11 +4,13 @@
 # 2. sed 'regex'
 # 3. git add , committ 'cleaned from google tracking links'
 
-%s#\vhttps://www.google.com/url\?q\=(.+)(\&sa.+\)){0}#\1)#g
-
-%s#\vhttps://www.google.com/url\?q\=#\1#g
+%s#\vhttps://www.google.com/url\?q\=(.{-})(\&sa.{-}\)){1}#\1)#g
 
 
-# dirty links
-[Sito del progetto](https://www.google.com/url?q=http://www.iltucci.com/blog/la-domotica-fai-da-me/&sa=D&ust=1465242204038000&usg=AFQjCNG0Z2a4IJ25Hc3fwO41DVuL1PMRKQ) e [Repository GitHub](https://www.google.com/url?q=https://github.com/cesco78/domotica-telegram&sa=D&ust=1465242204039000&usg=AFQjCNGbxIaZ-M5RBqSRjWxdy_CuVP7gAw)
+# dirty links example
+[Sito del progetto](https://www.google.com/url?q=http://www.iltucci.com/blog/la-domotica-fai-da-me/&sa=D&ust=1465242204038000&usg=AFQjCNG0Z2a4IJ25Hc3fwO41DVuL1PMRKQ) e [Sito del progetto](https://www.google.com/url?q=http://www.iltucci.com/blog/la-domotica-fai-da-me/&sa=D&ust=1465242204038000&usg=AFQjCNG0Z2a4IJ25Hc3fwO41DVuL1PMRKQ) 
+
+# cleaned link 
+[Sito del progetto](http://www.iltucci.com/blog/la-domotica-fai-da-me/)
+
 
