d2fc59a27fdce4853a8aaaaca7fd5b2a631b35ad
kidpixo <kidpixo@gmail.com>
2016-09-19 21:32:46 +0200

    added a regex to clean new pandoc stuff in {}

--- _scripts/clean_posts.sh
+++ _scripts/clean_posts.sh
@@ -6,11 +6,11 @@
 
 %s#\vhttps://www.google.com/url\?q\=(.{-})(\&sa.{-}\)){1}#\1)#g
 
-
 # dirty links example
 [Sito del progetto](https://www.google.com/url?q=http://www.iltucci.com/blog/la-domotica-fai-da-me/&sa=D&ust=1465242204038000&usg=AFQjCNG0Z2a4IJ25Hc3fwO41DVuL1PMRKQ) e [Sito del progetto](https://www.google.com/url?q=http://www.iltucci.com/blog/la-domotica-fai-da-me/&sa=D&ust=1465242204038000&usg=AFQjCNG0Z2a4IJ25Hc3fwO41DVuL1PMRKQ) 
 
 # cleaned link 
 [Sito del progetto](http://www.iltucci.com/blog/la-domotica-fai-da-me/)
 
-
+# new pandoc put classes like {.c 0}, delete them:
+%s/{.\{-}}//g
